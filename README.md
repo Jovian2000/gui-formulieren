49917dcf1d7e4e883835696c83259b986c666623
Jovian <99047257@mydavinci.nl>
2022-01-10 13:11:30 +0100

    Update readme + dambord toegevoegd

--- README.md
+++ README.md
@@ -85,3 +85,33 @@ autoClickBox = tk.Checkbutton(
     )
 autoClickBox.pack()
 ```
+## F1.8.03.L4 - een Dambord
+Dambord gemaakt
+``` python
+import tkinter as tk
+
+root = tk.Tk()
+root.geometry("800x800")
+root.title("Dambord")
+
+frame = tk.Frame(root)
+
+v = False
+for j in range(10):
+    if v == True:
+        v = False
+    else:
+        v = True
+    for o in range(10):
+        if v == True:
+            colour = "black"
+            v = False
+        else: 
+            colour = "white"
+            v = True
+        tile = tk.Label(frame, bg = colour, padx = 30, pady = 20)
+        tile.grid(row = j, column = o)
+frame.pack(expand = True)
+
+root.mainloop()
+```
--- dambord.py
+++ dambord.py
@@ -1,10 +1,26 @@
 import tkinter as tk
 
 root = tk.Tk()
-root.geometry("500x500")
+root.geometry("800x800")
 root.title("Dambord")
 
-blackFrame = tk.Label(root, bg = "black")
-blackFrame.grid(column = 0, row = 0,  ipadx = 10, ipady = 10)
+frame = tk.Frame(root)
+
+v = False
+for j in range(10):
+    if v == True:
+        v = False
+    else:
+        v = True
+    for o in range(10):
+        if v == True:
+            colour = "black"
+            v = False
+        else: 
+            colour = "white"
+            v = True
+        tile = tk.Label(frame, bg = colour, padx = 30, pady = 20)
+        tile.grid(row = j, column = o)
+frame.pack(expand = True)
 
 root.mainloop()
